b9fdfdc8cf4ab8e15e601c3ba7cc24409a8f4e83
systopia <endres@systopia.de>
2014-08-21 12:38:32 +0200

    basic structure

---
+++ donrec.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once 'donrec.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ */
+function donrec_civicrm_config(&$config) {
+  _donrec_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ */
+function donrec_civicrm_xmlMenu(&$files) {
+  _donrec_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ */
+function donrec_civicrm_install() {
+  return _donrec_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ */
+function donrec_civicrm_uninstall() {
+  return _donrec_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ */
+function donrec_civicrm_enable() {
+  return _donrec_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ */
+function donrec_civicrm_disable() {
+  return _donrec_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ */
+function donrec_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _donrec_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ */
+function donrec_civicrm_managed(&$entities) {
+  return _donrec_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ */
+function donrec_civicrm_caseTypes(&$caseTypes) {
+  _donrec_civix_civicrm_caseTypes($caseTypes);
+}
