1854245396cb5f907c38a338cd9dea9753ec5125
thomas <thomaslfuss@gmx.de>
2014-10-13 09:28:24 +0200

    bugfix: Create.php rose an error on intersection-error but no snapshot
    This is a general problem: There are already two intersecting snapshots.
    And the user tries to create a third one.
    The third one won't be created. But how should we deal with the existing
    ones? Delete both? Ask the user for deleting both? And what if one of
    them belongs to another user?

--- CRM/Donrec/Form/Task/Create.php
+++ CRM/Donrec/Form/Task/Create.php
@@ -111,16 +111,17 @@ class CRM_Donrec_Form_Task_Create extends CRM_Core_Form {
 
     // try to create a snapshot and redirect depending on the result (conflict)
     $result = CRM_Donrec_Logic_Snapshot::create($contributionIds, CRM_Core_Session::getLoggedInContactID());
+    $sid = empty($result['snapshot'])?NULL:$result['snapshot']->getId();
 
     if (!empty($result['intersection_error'])) {
       CRM_Core_Session::singleton()->pushUserContext(
-        CRM_Utils_System::url('civicrm/donrec/task', 'conflict=1' . 'sid=' . $result['snapshot']->getId() . '&ccount=1'));
+        CRM_Utils_System::url('civicrm/donrec/task', 'conflict=1' . 'sid=' . $sid . '&ccount=1'));
     }elseif (empty($result['snapshot'])) {
       CRM_Core_Session::setStatus(ts('There are no contributions for this contact that can be used to issue donation receipts.'), ts('Warning'), 'warning');
       CRM_Utils_System::redirect(CRM_Utils_System::url('civicrm/contact/view', "reset=1&cid=$contactId"));
     }else{
       CRM_Core_Session::singleton()->pushUserContext(
-        CRM_Utils_System::url('civicrm/donrec/task', 'sid=' . $result['snapshot']->getId() . '&ccount=1')
+        CRM_Utils_System::url('civicrm/donrec/task', 'sid=' . $sid . '&ccount=1')
       );
     }
   }
