01bea784e5db9bf49ecb34f205762d7d6bc8da26
thomas <thomaslfuss@gmx.de>
2014-10-27 13:49:45 +0100

    don't use Tab.php for receipt-views

--- CRM/Donrec/Page/Tab.php
+++ CRM/Donrec/Page/Tab.php
@@ -4,40 +4,24 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Donrec_Page_Tab extends CRM_Core_Page {
   function run() {
+    $contact_id = empty($_REQUEST['cid']) ? NULL : $_REQUEST['cid'];
 
-    if (CRM_Utils_Array::value('view', $_REQUEST, False)) {
-      $rid = empty($_REQUEST['rid']) ? NULL : $_REQUEST['rid'];
-      if (empty($rid)) {
-        //TODO: ERROR
+    if($contact_id) {
+      $params = array();
+      $receipts = CRM_Donrec_Logic_Receipt::getReceiptsForContact($contact_id, $params);
+      $display_receipts = array();
+      foreach ($receipts as $rec) {
+        $display_receipts[$rec->getId()] = $rec->getDisplayProperties();
       }
-      $receipt = new CRM_Donrec_Logic_Receipt($rid);
-      $file_url = $receipt->viewPdf();
-
-      //redirect to the pdf
-      CRM_Utils_System::redirect($file_url);
-    } else {
-      $contact_id = empty($_REQUEST['cid']) ? NULL : $_REQUEST['cid'];
-
-      if($contact_id) {
-        $params = array();
-        $receipts = CRM_Donrec_Logic_Receipt::getReceiptsForContact($contact_id, $params);
-        $display_receipts = array();
-        $view_url = array();
-        foreach ($receipts as $rec) {
-          $rid = $rec->getId();
-          $display_receipts[$rid] = $rec->getDisplayProperties();
-          $display_receipts[$rid]['view_url'] = CRM_Utils_System::url('civicrm/donrec/tab', "view=1&rid=$rid");
-        }
-        $this->assign('cid', $contact_id);
-        $this->assign('display_receipts', $display_receipts);
-        $this->assign('view_url', $view_url);
-      }
-
-      // admin only
-      $this->assign('is_admin', CRM_Core_Permission::check('administer CiviCRM'));
-      // do we keep original pdf files?
-      $this->assign('store_pdf', CRM_Donrec_Logic_Settings::saveOriginalPDF());
+      $this->assign('cid', $contact_id);
+      $this->assign('display_receipts', $display_receipts);
     }
+
+    // admin only
+    $this->assign('is_admin', CRM_Core_Permission::check('administer CiviCRM'));
+    // do we keep original pdf files?
+    $this->assign('store_pdf', CRM_Donrec_Logic_Settings::saveOriginalPDF());
+
   	parent::run();
   }
 }
