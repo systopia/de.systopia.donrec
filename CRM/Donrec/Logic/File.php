e0692cf6f0c5c4174ef7cb50068b197ca14b96a5
systopia <endres@systopia.de>
2014-10-30 08:03:56 +0100

    Created permanent files download

--- CRM/Donrec/Logic/File.php
+++ CRM/Donrec/Logic/File.php
@@ -34,14 +34,54 @@ class CRM_Donrec_Logic_File {
    * This function will take any file and make it permanently 
    * available as a CiviCRM File entity.
    * 
-   * @param filepath      where to find the file
-   * @param deleteSource  if true, the original file will be deleted
+   * @param path          where to find the file
+   * @param contact_id    which contact to connect to
+   * 
    * @param mimetype      the document's MIME type. Autodetect if null
    * 
-   * @return a CRM_Core_BAO_File object
+   * @return an array containing the created file object, including a generated url
    */
-  public static function createPermanentFile($filepath, $deleteSource = true, $mimetype = null) {
-    // TODO: Implement
-    return null;
+  public static function createPermanentFile($path, $name = null, $contact_id, $mimetype = null, $description = '') {
+    $config = CRM_Core_Config::singleton();
+    if (!file_exists($path)) return null;
+
+    // TODO: check if a file object already exists?
+
+    // move file to a permanent folder
+    $newPath = $config->customFileUploadDir . basename($path);
+    rename($path, $newPath);    
+
+    // find mime type
+    if (empty($mimetype)) {
+      $mimetype = mime_content_type($newPath);
+    }
+
+    // create the file object
+    if (empty($description)) $description = $name;
+    $file = civicrm_api('File', 'create', array(
+      'version'       => 3,
+      'uri'           => basename($newPath),
+      'mime_type'     => $mimetype,
+      'description'   => $description,
+      ));
+
+    if (!empty($file['is_error'])) {
+      error_log("de.systopia.donrec: couldn't create file object - " . $file['error_message']);
+      return null;
+    }
+
+    if ($contact_id) {
+      // link the file to a contact  (there is no API call for this...)
+      $entityFile = new CRM_Core_DAO_EntityFile();
+      $entityFile->file_id = $file['id'];
+      $entityFile->entity_id = $contact_id;
+      $entityFile->entity_table = 'civicrm_contact';
+      $entityFile->save();
+    }
+
+    // create URL
+    $file['url'] = CRM_Utils_System::url("civicrm/file", "reset=1&id=" . $file['id'] . "&eid=$contact_id");
+    
+    return $file;
   }
 }
