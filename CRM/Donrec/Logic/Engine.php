08e9ae059d06be3fa28052728eed360779a1a524
Niko Bochan <bochan@systopia.de>
2014-11-13 16:49:23 +0100

    fixes #1465

--- CRM/Donrec/Logic/Engine.php
+++ CRM/Donrec/Logic/Engine.php
@@ -193,7 +193,7 @@ class CRM_Donrec_Logic_Engine {
               // get pdf file name from snapshot line
               $pdf_file = $this->getPDF($line_id);
               if ($pdf_file) {
-                $tempReceipt = $this->snapshot->getSnapshotReceipt($line_id, $is_test);
+                $tempReceipt = $this->snapshot->getSnapshotReceipt(array($line_id), $is_test);
                 $contact_id =  $tempReceipt->getContactID();
                 $file = CRM_Donrec_Logic_File::createPermanentFile($pdf_file, basename($pdf_file), $contact_id);
                 if (!empty($file)) {
