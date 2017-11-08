16c5b179d87936eda44ff40a0294a90c109208a0
ruben <r.pineda@ixiam.com>
2017-11-08 11:00:46 +0100

    Add language fr

---
+++ CRM/Utils/Lang/Fr/Fr.php
@@ -0,0 +1,19 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA Donation Receipts Extension                   |
+| Copyright (C) 2013-2016 SYSTOPIA                       |
+| Author: Luciano Spiegel                                |
+| http://www.ixiam.com/                                  |
++--------------------------------------------------------+
+| License: AGPLv3, see LICENSE file                      |
++--------------------------------------------------------*/
+
+/**
+ * This class holds Spanish language helper functions
+ */
+class CRM_Utils_Lang_Fr_Fr {
+  
+  static public function toWords($number, $miMoneda = null) {
+    return "";
+  }
+}
