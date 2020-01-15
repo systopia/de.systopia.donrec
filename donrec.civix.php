dddd79a6a815cb4de2615f3b747a16696d4f6df9
Jens Schuppe <schuppe@systopia.de>
2020-01-15 16:38:33 +0100

    [#92] Update civix file

--- donrec.civix.php
+++ donrec.civix.php
@@ -263,6 +263,7 @@ function _donrec_civix_find_files($dir, $pattern) {
  */
 function _donrec_civix_civicrm_managed(&$entities) {
   $mgdFiles = _donrec_civix_find_files(__DIR__, '*.mgd.php');
+  sort($mgdFiles);
   foreach ($mgdFiles as $file) {
     $es = include $file;
     foreach ($es as $e) {
