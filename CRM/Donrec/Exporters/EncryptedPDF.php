03cb4deaf09824069582498afe9ed9b5f928b702
Benni Baermann <baermann@systopia.de>
2022-12-10 15:00:00 +0100

    basic checkRequirements added to EncryptedPDF

--- CRM/Donrec/Exporters/EncryptedPDF.php
+++ CRM/Donrec/Exporters/EncryptedPDF.php
@@ -24,10 +24,40 @@ abstract class CRM_Donrec_Exporters_EncryptedPDF extends CRM_Donrec_Exporters_Ba
     return $password;
   }
 
+  // checks if pdftk is installed, but only if encryption is activates in the profile
+  public function checkRequirements($profile = NULL): array {
+      $result = array();
+      $result['is_error'] = FALSE;
+      $result['messgae'] ='';
+      if($profile->getDataAttribute('enable_encryption')){
+        // $result['is_error'] = TRUE;
+        // $result['message'] = 'just for testing';
+
+        // Check if encryption tool is available.
+        $path = CRM_Donrec_Logic_Settings::get('encryption_command');
+        if (empty($path)) {
+          $result['is_eror'] = TRUE;
+          $result['message'] = 'no path to encryption tool given. please check the donrec settings';
+        }else{
+
+          // "Ping" encyrption command.
+          $cmd = escapeshellcmd($path) . ' 2>&1';
+          exec($cmd, $output, $ret_status);
+
+          if($ret_status != 0){
+            $result['is_error'] = TRUE;
+            $result['message'] ='execution of ' . $path . ' failed.';
+          }
+        }
+      }
+      return $result;
+  }
+
   // encrypt the given file if the setting in the profile says so
   protected function encrypt_file($file, $snapshot_receipt): void {
 
     if ($snapshot_receipt->getProfile()->getDataAttribute('enable_encryption')) {
+
       $password = $this->generate_password();
       $cmd = CRM_Donrec_Logic_Settings::get('encryption_command');
       $tmpfile = $file . "_tmp";
