5fc1ae64f67077c339276dbdee36089476963c73
Jens Schuppe <schuppe@systopia.de>
2023-02-08 11:42:43 +0100

    Upgrade Civix code with Civix version 22.10.0

--- donrec.civix.php
+++ donrec.civix.php
@@ -24,7 +24,7 @@ class CRM_Donrec_ExtensionUtil {
    *   Translated text.
    * @see ts
    */
-  public static function ts($text, $params = []) {
+  public static function ts($text, $params = []): string {
     if (!array_key_exists('domain', $params)) {
       $params['domain'] = [self::LONG_NAME, NULL];
     }
@@ -41,7 +41,7 @@ class CRM_Donrec_ExtensionUtil {
    *   Ex: 'http://example.org/sites/default/ext/org.example.foo'.
    *   Ex: 'http://example.org/sites/default/ext/org.example.foo/css/foo.css'.
    */
-  public static function url($file = NULL) {
+  public static function url($file = NULL): string {
     if ($file === NULL) {
       return rtrim(CRM_Core_Resources::singleton()->getUrl(self::LONG_NAME), '/');
     }
@@ -147,7 +147,7 @@ function _donrec_civix_civicrm_postInstall() {
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
  */
-function _donrec_civix_civicrm_uninstall() {
+function _donrec_civix_civicrm_uninstall(): void {
   _donrec_civix_civicrm_config();
   if ($upgrader = _donrec_civix_upgrader()) {
     $upgrader->onUninstall();
@@ -159,7 +159,7 @@ function _donrec_civix_civicrm_uninstall() {
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
-function _donrec_civix_civicrm_enable() {
+function _donrec_civix_civicrm_enable(): void {
   _donrec_civix_civicrm_config();
   if ($upgrader = _donrec_civix_upgrader()) {
     if (is_callable([$upgrader, 'onEnable'])) {
@@ -175,7 +175,7 @@ function _donrec_civix_civicrm_enable() {
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
  * @return mixed
  */
-function _donrec_civix_civicrm_disable() {
+function _donrec_civix_civicrm_disable(): void {
   _donrec_civix_civicrm_config();
   if ($upgrader = _donrec_civix_upgrader()) {
     if (is_callable([$upgrader, 'onDisable'])) {
