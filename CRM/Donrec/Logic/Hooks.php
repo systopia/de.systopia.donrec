12b266e6f3b0d875770d01b8a9b448acf46ab557
Jens Schuppe <schuppe@systopia.de>
2023-12-12 12:38:14 +0100

    [#186] Fix hook "should be updated to pass an array of parameter names" deprecation notice

--- CRM/Donrec/Logic/Hooks.php
+++ CRM/Donrec/Logic/Hooks.php
@@ -29,9 +29,9 @@ class CRM_Donrec_Logic_Hooks {
   static function donationReceiptTokenValues(&$values) {
     if (version_compare(CRM_Utils_System::version(), '4.5', '<'))
     {
-      return CRM_Utils_Hook::singleton()->invoke(1, $values, self::$null, self::$null, self::$null, self::$null, $hook = 'civicrm_donationReceiptTokenValues');
+      return CRM_Utils_Hook::singleton()->invoke(['values'], $values, self::$null, self::$null, self::$null, self::$null, $hook = 'civicrm_donationReceiptTokenValues');
     } else {
-      return CRM_Utils_Hook::singleton()->invoke(1, $values, self::$null, self::$null, self::$null, self::$null, self::$null, $hook = 'civicrm_donationReceiptTokenValues');
+      return CRM_Utils_Hook::singleton()->invoke(['values'], $values, self::$null, self::$null, self::$null, self::$null, self::$null, $hook = 'civicrm_donationReceiptTokenValues');
     }
   }
 
