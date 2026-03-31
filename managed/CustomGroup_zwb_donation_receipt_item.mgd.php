36573bae812e509181ed842e66053e5fb7e0ab7d
Dominic Tubach <tubach@systopia.de>
2026-03-31 12:27:13 +0200

    fixup! Use managed entities for option groups and custom groups

--- managed/CustomGroup_zwb_donation_receipt_item.mgd.php
+++ managed/CustomGroup_zwb_donation_receipt_item.mgd.php
@@ -277,67 +277,6 @@ return [
     ],
   ],
   [
-    'name' => 'OptionGroup_donrec_type',
-    'entity' => 'OptionGroup',
-    'cleanup' => 'unused',
-    'update' => 'unmodified',
-    'params' => [
-      'version' => 4,
-      'values' => [
-        'name' => 'donrec_type',
-        'title' => E::ts('type'),
-        'option_value_fields' => [
-          'name',
-          'label',
-          'description',
-        ],
-      ],
-      'match' => [
-        'name',
-      ],
-    ],
-  ],
-  [
-    'name' => 'OptionGroup_donrec_type_OptionValue_SINGLE',
-    'entity' => 'OptionValue',
-    'cleanup' => 'unused',
-    'update' => 'unmodified',
-    'params' => [
-      'version' => 4,
-      'values' => [
-        'option_group_id.name' => 'donrec_type',
-        'label' => E::ts('single'),
-        'value' => 'SINGLE',
-        'name' => 'SINGLE',
-      ],
-      'match' => [
-        'option_group_id',
-        'name',
-        'value',
-      ],
-    ],
-  ],
-  [
-    'name' => 'OptionGroup_donrec_type_OptionValue_BULK',
-    'entity' => 'OptionValue',
-    'cleanup' => 'unused',
-    'update' => 'unmodified',
-    'params' => [
-      'version' => 4,
-      'values' => [
-        'option_group_id.name' => 'donrec_type',
-        'label' => E::ts('bulk'),
-        'value' => 'BULK',
-        'name' => 'BULK',
-      ],
-      'match' => [
-        'option_group_id',
-        'name',
-        'value',
-      ],
-    ],
-  ],
-  [
     'name' => 'CustomGroup_zwb_donation_receipt_item_CustomField_type',
     'entity' => 'CustomField',
     'cleanup' => 'unused',
