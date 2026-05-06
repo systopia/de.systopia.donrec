0018ffed9b39bd3c1e751f95e99f62f37b8479b2
Dominic Tubach <tubach@systopia.de>
2026-05-06 10:12:51 +0200

    Set version to 2.4.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.donrec',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6f74e1de4e2cc53fa7220da643449b8702b1e8c5',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '2.4.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.donrec' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6f74e1de4e2cc53fa7220da643449b8702b1e8c5',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '2.4.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
