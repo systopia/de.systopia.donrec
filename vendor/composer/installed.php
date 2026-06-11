08d97efa032e75cd71dacd4010428030e075bd1e
Jens Schuppe <schuppe@systopia.de>
2026-06-11 13:21:13 +0200

    Set version to 2.4.1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.donrec',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '46a8db90fead16121738416a82f58f70b52090b3',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '2.5.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.donrec' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '46a8db90fead16121738416a82f58f70b52090b3',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '2.5.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
